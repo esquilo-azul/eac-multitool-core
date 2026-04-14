7d80d98299ad0cff095ec5c8f27b282de14cc2ae
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-04-14 16:01:28 -0300

    Sub[sub/programeiro]: add '0.4.0'.

---
+++ sub/programeiro/completion.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ -z $PALIAS ]]; then
+  PALIAS="programeiro"
+fi
+FUNCTION_NAME="_p_completion_$PALIAS"
+
+_p_completion() {
+  local cur
+  COMPREPLY=()
+  cur=${COMP_WORDS[COMP_CWORD]}
+  case "$COMP_CWORD" in
+    1)
+    COMPREPLY=( $( p_completion_search $cur ) )
+    ;;
+
+    *)
+    COMPREPLY=( $( compgen -o default -- $cur ) )
+    ;;
+  esac
+  return 0
+}
+
+eval "${FUNCTION_NAME}() { source '$PROOT/lib.sh'; set +u; set +e; _p_completion; return 0; }"
+complete -F "$FUNCTION_NAME" -o nospace "$PALIAS"
