304b80d09a1f7c9ed47542615736ea9b6d066be1
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-06-12 16:35:58 -0300

    programeiro_run_file(): runner by mime type.

--- sub/programeiro/lib/programeiro_run_file.sh
+++ sub/programeiro/lib/programeiro_run_file.sh
@@ -1,6 +1,12 @@
 function programeiro_run_file() {
   local FILE="$1"
   shift
-  "${FILE}" "$@"
+  var_set_by MIME_TYPE file_mime_type "${FILE}"
+  var_set_by RUNNER programeiro_mime_type_get "${MIME_TYPE}"
+  if [[ -n "${RUNNER}" ]]; then
+    "${RUNNER}" "${FILE}" "$@"
+  else
+    "${FILE}" "$@"
+  fi
 }
 export -f programeiro_run_file
