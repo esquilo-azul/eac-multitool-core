c13ad6aa0e3428acfc1af99cc69d527fedb8c933
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-06-17 17:11:04 -0300

    programeiro_run_file.sh(): runner by extension.

--- sub/programeiro/lib/programeiro_run_file.sh
+++ sub/programeiro/lib/programeiro_run_file.sh
@@ -3,6 +3,10 @@ function programeiro_run_file() {
   shift
   var_set_by MIME_TYPE file_mime_type "${FILE}"
   var_set_by RUNNER programeiro_mime_type_get "${MIME_TYPE}"
+  if [[ -z "${RUNNER}" ]]; then
+    var_set_by EXTENSION path_extension "${FILE}"
+    var_set_by RUNNER programeiro_extension_get "${EXTENSION}"
+  fi
   if [[ -n "${RUNNER}" ]]; then
     "${RUNNER}" "${FILE}" "$@"
   else
