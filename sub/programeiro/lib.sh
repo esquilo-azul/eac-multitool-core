2fa1a26b6a546a4b4df21088d47f66290752b82c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-06-23 22:18:00 -0300

    lib.sh: source "eac-bash-lib/init.sh".

--- sub/programeiro/lib.sh
+++ sub/programeiro/lib.sh
@@ -6,7 +6,23 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 PROGRAMEIRO_ROOT_ROOT="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
+export EACBASHLIB_NOT_FOUND_ERROR=7
+export EACBASHLIB_SOURCE_URL='https://github.com/esquilo-azul/eac-bash-lib'
 export PROGRAMEIRO_NOT_FOUND_ERROR=6
+
+if [[ ! -v 'EACBASHLIB_ROOT' ]]; then
+  export EACBASHLIB_ROOT="$(dirname "$PROGRAMEIRO_ROOT_ROOT")/eac-bash-lib"
+fi
+export EACBASHLIB_RC="${EACBASHLIB_ROOT}/init.sh"
+
+if [[ ! -f "$EACBASHLIB_RC" ]]; then
+  >&2 printf "%s\n" "\"$EACBASHLIB_RC\" not found."
+  >&2 printf "%s %s\n" "You need to have a copy of ${EACBASHLIB_SOURCE_URL} in"
+    "\"$EACBASHLIB_ROOT\" or in directory pointed by \$EACBASHLIB_ROOT." \
+  exit $ERROR_EACBASHLIB_NOT_FOUND
+fi
+
+source "${EACBASHLIB_RC}"
 
 for file in "${PROGRAMEIRO_ROOT_ROOT}/lib/"*.sh; do
   source "$file"
